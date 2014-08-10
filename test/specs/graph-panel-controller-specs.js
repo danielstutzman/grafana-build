7367a0fd20b10dedc49d28f127cead86542ee4a4
Daniel Stutzman <dtstutz@gmail.com>
2014-08-10 10:58:14 -0600

    Start with grafana-1.7.0-rc1.tar.gz download

---
+++ test/specs/graph-panel-controller-specs.js
@@ -0,0 +1,3 @@
+/*! grafana - v1.7.0-rc1 - 2014-08-05
+ * Copyright (c) 2014 Torkel Ödegaard; Licensed Apache License */
+
---
+++ test/specs/graphiteTargetCtrl-specs.js
@@ -0,0 +1,4 @@
+/*! grafana - v1.7.0-rc1 - 2014-08-05
+ * Copyright (c) 2014 Torkel Ödegaard; Licensed Apache License */
+
+define([],function(){describe("graphiteTargetCtrl",function(){var a;beforeEach(module("grafana.services")),beforeEach(module(function(a){a.value("filterSrv",{})})),beforeEach(inject(function(b,c){a=b({$scope:c.$new()})})),describe("init",function(){beforeEach(function(){})})})});
